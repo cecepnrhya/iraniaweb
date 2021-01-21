a47e123853178c57c56bae274a6e64c1b05d8d96
cecepnrhya <46098300+cecepnrhya@users.noreply.github.com>
2021-01-21 19:55:43 +0700

    Add files via upload

---
+++ koneksidatabase.php
@@ -0,0 +1,12 @@
+<?php
+
+//deklasrasi variabel
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "skripsi";    
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+
+?>
